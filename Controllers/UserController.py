96358ef22d108f711dae0e7c63379dae72060e16
matheused <matheus.lafeta.moc@gmail.com>
2023-04-16 21:54:19 -0300

    Integrating UserController.py to the DB

--- Controllers/UserController.py
+++ Controllers/UserController.py
@@ -1,32 +1,45 @@
-import os
-import pickle
-
+import sqlite3
 from Model.User import User
-
+import _md5
 
 class UserController:
     def __init__(self):
-        self.users = []
-        self.load_users()
+        self.conn = sqlite3.connect('../Database/Migrations/diary.db')
+        self.create_user_table()
+
+    def create_user_table(self):
+        cursor = self.conn.cursor()
+        cursor.execute('''CREATE TABLE IF NOT EXISTS travellers (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        username TEXT NOT NULL,
+                        name TEXT NOT NULL,
+                        password TEXT NOT NULL
+                                            )''')
+        self.conn.commit()
 
     def register_user(self, username, name, password):
         # Check if the username is already taken
-        if any(user.username == username for user in self.users):
+        cursor = self.conn.cursor()
+        cursor.execute('SELECT * FROM travellers WHERE username=?', (username,))
+        result = cursor.fetchone()
+        if result is not None:
+            print("deu nao")
             return False
 
-        # Create a new user and add it to the list
-        user = User(username, name, password)
-        self.users.append(user)
-        self.save_users()
+        # Create a new user and add it to the database
+        cursor.execute('INSERT INTO travellers (username, name, password) VALUES (?, ?, ?)',
+                       (username, name, password))
+        self.conn.commit()
         return True
 
-    def save_users(self):
-        # Save the users to a file using the pickle package
-        with open('users.pickle', 'wb') as f:
-            pickle.dump(self.users, f)
+    def get_user_by_username(self, username):
+        # Retrieve a user by username
+        cursor = self.conn.cursor()
+        cursor.execute('SELECT * FROM viajantes WHERE username=?', (username,))
+        result = cursor.fetchone()
+        if result is None:
+            return None
+        return User(result[1], result[2], result[3])
 
-    def load_users(self):
-        # Load the users from the file if it exists
-        if os.path.exists('users.pickle'):
-            with open('users.pickle', 'rb') as f:
-                self.users = pickle.load(f)
+a = UserController()
+a.register_user('asad','123','asdv')
